3b57f526ac540100758aa4485c6cebbd51af01d8
Muzaffer YILDIRIM <mail@muzaffer.dev>
2024-09-14 03:58:26 +0300

    fix(18): Fixed Create Message response createdAt

--- internal/messages/repository.go
+++ internal/messages/repository.go
@@ -16,10 +16,10 @@ func NewRepository(db *pgxpool.Pool) *Repository {
 }
 
 func (r *Repository) Create(ctx context.Context, message *CreateMessageRequest) (*Message, error) {
-	query := `INSERT INTO messages (recipient, content) VALUES ($1, $2) RETURNING id, recipient, content, status`
+	query := `INSERT INTO messages (recipient, content) VALUES ($1, $2) RETURNING id, recipient, content, status, created_at`
 
 	newMessage := &Message{}
-	err := r.db.QueryRow(ctx, query, message.Recipient, message.Content).Scan(&newMessage.ID, &newMessage.Recipient, &newMessage.Content, &newMessage.Status)
+	err := r.db.QueryRow(ctx, query, message.Recipient, message.Content).Scan(&newMessage.ID, &newMessage.Recipient, &newMessage.Content, &newMessage.Status, &newMessage.CreatedAt)
 	if err != nil {
 		return nil, err
 	}
