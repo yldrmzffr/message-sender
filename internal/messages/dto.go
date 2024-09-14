abf54c7f84e834ce63814d48d085ab867e30133b
Muzaffer YILDIRIM <mail@muzaffer.dev>
2024-09-14 04:15:13 +0300

    fix(20): GetSentMessages

--- internal/messages/dto.go
+++ internal/messages/dto.go
@@ -10,9 +10,10 @@ type CreateMessageRequest struct {
 // MessageResponse represents the response body for message operations
 // swagger:response MessageResponse
 type MessageResponse struct {
-	ID        uint   `json:"id" example:"1"`
-	Recipient string `json:"recipient" example:"+905380000000"`
-	Content   string `json:"content" example:"Hello, World!"`
-	Status    Status `json:"status" example:"sent"`
-	CreatedAt string `json:"createdAt" example:"2021-01-01T00:00:00Z"`
+	ID          uint   `json:"id" example:"1"`
+	Recipient   string `json:"recipient" example:"+905380000000"`
+	Content     string `json:"content" example:"Hello, World!"`
+	Status      Status `json:"status" example:"sent"`
+	CreatedAt   string `json:"createdAt" example:"2021-01-01T00:00:00Z"`
+	CompletedAt string `json:"completedAt,omitempty" example:"2021-01-01T00:00:00Z"`
 }
--- internal/messages/repository.go
+++ internal/messages/repository.go
@@ -26,3 +26,26 @@ func (r *Repository) Create(ctx context.Context, message *CreateMessageRequest) 
 
 	return newMessage, nil
 }
+
+func (r *Repository) GetSentMessages(ctx context.Context) ([]*Message, error) {
+	query := `SELECT id, recipient, content, status, created_at, completed_at FROM messages WHERE status = 'sent'`
+
+	rows, err := r.db.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	messages := make([]*Message, 0)
+	for rows.Next() {
+		message := &Message{}
+		err := rows.Scan(&message.ID, &message.Recipient, &message.Content, &message.Status, &message.CreatedAt, &message.CompletedAt)
+		if err != nil {
+			return nil, err
+		}
+		messages = append(messages, message)
+	}
+
+	return messages, nil
+
+}
